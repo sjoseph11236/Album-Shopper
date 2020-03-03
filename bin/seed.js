34722af9e6601e60816ed189a35eed33833983d5
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-03-02 19:53:09 -0500

    chore: seed orderItems

--- bin/seed.js
+++ bin/seed.js
@@ -2,6 +2,7 @@ const { db, User, Album, Order, OrderItem } = require('../server/db');
 const users = require('./data/user.json');
 const albums = require('./data/album.json');
 const orders = require('./data/order.json');
+const orderItems = require('./data/orderItem.json');
 const {green, red} = require('chalk');
 
 const seed = async() => {
@@ -10,7 +11,8 @@ const seed = async() => {
   await User.bulkCreate(users);
   await Album.bulkCreate(albums);
   await Order.bulkCreate(orders);
-  
+  await OrderItem.bulkCreate(orderItems);
+
   console.log(green('Seeding success!'));
   db.close();
 }
